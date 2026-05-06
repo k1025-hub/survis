d746130e92be059d50217abcbde0b532cddb0442
k1025-hub <473744476@qq.com>
2026-05-06 19:41:46 +0800

    Add new bibliographic entries to bib.js
    Added new bibliographic entries for various research papers in the field of vehicle routing and reinforcement learning.

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,118 @@
-﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
-        "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
-    }
-};
+{
+  "Vinyals2015Pointer": {
+    "type": "inproceedings",
+    "title": "{{Pointer Networks}}",
+    "author": "Vinyals, Oriol and Fortunato, Meire and Jaitly, Navdeep",
+    "year": "2015",
+    "booktitle": "Advances in Neural Information Processing Systems",
+    "volume": "28",
+    "pages": "2692--2700",
+    "keywords": "type:construction, paradigm:supervised-learning, variant:TSP, architecture:pointer-network",
+    "url": "https://papers.nips.cc/paper/5866-pointer-networks",
+    "abstract": "A neural architecture that learns to output permutations of variable-length input sequences, trained supervisedly on TSP instances using optimal solutions from Concorde."
+  },
+  "Nazari2018Reinforcement": {
+    "type": "inproceedings",
+    "title": "{{Reinforcement Learning for Solving the Vehicle Routing Problem}}",
+    "author": "Nazari, Mohammadreza and Oroojlooy, Afshin and Snyder, Lawrence and Takac, Martin",
+    "year": "2018",
+    "booktitle": "Advances in Neural Information Processing Systems",
+    "volume": "31",
+    "pages": "9839--9849",
+    "keywords": "type:construction, paradigm:reinforcement-learning, variant:CVRP, architecture:actor-critic",
+    "url": "https://dl.acm.org/doi/10.5555/3327546.3327651",
+    "abstract": "An actor-critic reinforcement learning approach for CVRP that replaces LSTM encoders with element-wise projections to handle dynamic node states."
+  },
+  "Deudon2018Learning": {
+    "type": "inproceedings",
+    "title": "{{Learning Heuristics for the TSP by Policy Gradient}}",
+    "author": "Deudon, Michel and Cournut, Pierre and Lacoste, Alexandre and Adulyasak, Yossiri and Rousseau, Louis-Martin",
+    "year": "2018",
+    "booktitle": "Integration of Constraint Programming, Artificial Intelligence, and Operations Research",
+    "pages": "170--181",
+    "keywords": "type:construction, paradigm:reinforcement-learning, variant:TSP, architecture:transformer",
+    "url": "https://doi.org/10.1007/978-3-319-93031-2_12",
+    "abstract": "A Transformer-based policy gradient method for TSP that uses multi-head self-attention for node encoding combined with 2-opt post-processing."
+  },
+  "Kool2019Attention": {
+    "type": "inproceedings",
+    "title": "{{Attention, Learn to Solve Routing Problems!}}",
+    "author": "Kool, Wouter and van Hoof, Herke and Welling, Max",
+    "year": "2019",
+    "booktitle": "International Conference on Learning Representations",
+    "keywords": "type:construction, paradigm:reinforcement-learning, variant:CVRP, architecture:transformer",
+    "url": "https://openreview.net/forum?id=ByxBFsRqYm",
+    "abstract": "A full encoder-decoder Transformer trained with a greedy rollout REINFORCE baseline, applied to TSP, CVRP, VRPTW, and the orienteering problem."
+  },
+  "Chen2019Learning": {
+    "type": "inproceedings",
+    "title": "{{Learning to Perform Local Rewriting for Combinatorial Optimization}}",
+    "author": "Chen, Xinyun and Tian, Yuandong",
+    "year": "2019",
+    "booktitle": "Advances in Neural Information Processing Systems",
+    "volume": "32",
+    "pages": "6278--6289",
+    "keywords": "type:construction, paradigm:reinforcement-learning, variant:CVRP, architecture:actor-critic",
+    "url": "https://proceedings.neurips.cc/paper/2019/hash/131f383b434fdf48079bff1e44e2d9a5-Abstract.html",
+    "abstract": "An iterative local rewriting framework where a region picker and rule picker are jointly trained via actor-critic RL to optimise CVRP and scheduling instances."
+  },
+  "Ghosal2020Distributionally": {
+    "type": "article",
+    "title": "{{The Distributionally Robust Chance-Constrained Vehicle Routing Problem}}",
+    "author": "Ghosal, Shubhechyya and Wiesemann, Wolfram",
+    "year": "2020",
+    "journal": "Operations Research",
+    "volume": "68",
+    "number": "3",
+    "pages": "716--732",
+    "keywords": "type:modelling, paradigm:robust-optimisation, variant:CVRP, architecture:branch-and-cut",
+    "url": "https://doi.org/10.1287/opre.2019.1924",
+    "abstract": "A distributionally robust chance-constrained formulation for CVRP under demand uncertainty, solved via branch-and-cut exploiting subadditivity of the ambiguity set."
+  },
+  "Ulmer2020Modeling": {
+    "type": "article",
+    "title": "{{On Modeling Stochastic Dynamic Vehicle Routing Problems}}",
+    "author": "Ulmer, Marlin W. and Goodson, Justin C. and Mattfeld, Dirk C. and Thomas, Barrett W.",
+    "year": "2020",
+    "journal": "EURO Journal on Transportation and Logistics",
+    "volume": "9",
+    "pages": "100008",
+    "keywords": "type:modelling, paradigm:dynamic-programming, variant:stochastic-VRP, architecture:MDP",
+    "url": "https://doi.org/10.1016/j.ejtl.2020.100008",
+    "abstract": "A generalised MDP framework for stochastic dynamic VRP that embeds explicit route plans into state and action spaces, bridging ADP theory with practical routing procedures."
+  },
+  "Lu2020Learning": {
+    "type": "inproceedings",
+    "title": "{{A Learning-Based Iterative Method for Solving Vehicle Routing Problems}}",
+    "author": "Lu, Hao and Zhang, Xingwen and Yang, Shu",
+    "year": "2020",
+    "booktitle": "International Conference on Learning Representations",
+    "keywords": "type:perturbation, paradigm:reinforcement-learning, variant:CVRP, architecture:operator-selection",
+    "url": "https://openreview.net/forum?id=BJe1334YDH",
+    "abstract": "An RL controller that adaptively selects improvement operators from a hand-designed pool, outperforming LKH3 and OR-Tools on CVRPLIB benchmarks."
+  },
+  "Hottung2020Neural": {
+    "type": "inproceedings",
+    "title": "{{Neural Large Neighborhood Search for the Capacitated Vehicle Routing Problem}}",
+    "author": "Hottung, Andre and Tierney, Kevin",
+    "year": "2020",
+    "booktitle": "Proceedings of the 24th European Conference on Artificial Intelligence",
+    "pages": "443--450",
+    "keywords": "type:perturbation, paradigm:deep-generative-model, variant:CVRP, architecture:CVAE",
+    "url": "https://doi.org/10.3233/FAIA200124",
+    "abstract": "A conditional variational autoencoder replaces the repair step in large neighbourhood search, enabling richer neighbourhood exploration for CVRP and SDVRP."
+  },
+  "Desaulniers2021Machine": {
+    "type": "article",
+    "title": "{{Machine-Learning-Based Column Selection for Column Generation}}",
+    "author": "Desaulniers, Guy and Lodi, Andrea and Morabit, Mouad",
+    "year": "2021",
+    "journal": "Transportation Science",
+    "volume": "55",
+    "number": "4",
+    "pages": "815--831",
+    "keywords": "type:perturbation, paradigm:supervised-learning, variant:VRPTW, architecture:GNN",
+    "url": "https://doi.org/10.1287/trsc.2021.1045",
+    "abstract": "A GNN trained supervisedly to predict which columns to add during column generation for VRPTW, achieving 20-30% computation time reductions on industrial instances."
+  }
+}
